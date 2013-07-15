752f58271c2f8f39c9a6263b54917c5f397500e2
Slippor <slippform@gmail.com>
2013-07-15 10:54:28 +0800

    添加一些文件。

---
+++ ShouxinServer/Controllers/AboutController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShouxinServer.Controllers
+{
+    public class AboutController : Controller
+    {
+        //
+        // GET: /Abount/
+        public ActionResult Index()
+        {
+            ViewBag.Message = "";
+
+            return View();
+        }
+    }
+}
---
+++ ShouxinServer/Controllers/MainController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShouxinServer.Controllers
+{
+    public class MainController : Controller
+    {
+        //
+        // GET: /Main/
+        public ActionResult Index()
+        {
+            ViewBag.Menu = new List<String> {
+				"我的手信","账户管理",
+				"交易记录","会员保障","应用中心"
+            };
+            return View();
+        }
+    }
+}
